6992a2a3f1f7f642a64998757044b2ef3a614038
ircube <ivo753@gmail.com>
2020-06-09 15:19:40 -0400

    Updates

---
+++ precache-manifest.571a0c5b69b174180154ae1adbdf12d6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ac85e56beee122a6a56079f15bb7100",
+    "url": "/gh-pages-react/index.html"
+  },
+  {
+    "revision": "e3453a8d49bffed708a1",
+    "url": "/gh-pages-react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "bc5a713a699aae8f1afc",
+    "url": "/gh-pages-react/static/js/2.a78d0eb3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gh-pages-react/static/js/2.a78d0eb3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3453a8d49bffed708a1",
+    "url": "/gh-pages-react/static/js/main.366b0ef0.chunk.js"
+  },
+  {
+    "revision": "ea729db8214cf96d812d",
+    "url": "/gh-pages-react/static/js/runtime-main.f39fc4b9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/gh-pages-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
